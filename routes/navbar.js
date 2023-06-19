f732c8f3906141b589589e688e76b6535b339c17
Anabelle <acolmena@stanford.edu>
2023-06-19 15:56:10 -0400

    UI/UX updates

--- routes/navbar.js
+++ routes/navbar.js
@@ -9,9 +9,9 @@ router.get('/who-is-behind', (req, res) => {
     res.render('navbar/about/whoIsBehind')
 })
 
-// router.get('/who-is-it-for', (req, res) => {
-//     res.render('navbar/about/whoIsItFor')
-// })
+router.get('/what-is-a-frame', (req, res) => {
+    res.render('navbar/about/whatWeMeanByFrame')
+})
 
 // Path to view our other tools
 router.get('/astrsc', (req, res) => {
